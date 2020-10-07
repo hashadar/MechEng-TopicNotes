1859010cc396dc798d2df92ae1bf041f448ca28d
Hasha Dar <pickupthehouse@gmail.com>
2020-10-07 23:00:14 +0100

    tutorial updated

--- MECH0011/MATLAB/Tutorial1.m
+++ MECH0011/MATLAB/Tutorial1.m
@@ -1,8 +1,12 @@
 %Hasha Dar
+clc
+clear
+close all
 
-[x,y]=meshgrid(-2:0.1:2,1:0.1:4);
+[x,y]=meshgrid(-2:0.5:2,1:0.5:4); %field
 
-u = zeros(40);
-v = (4.*log(y) -2.*y + 10);
+u = 0.*x; %x vector
+v = (4.*log(y) -2.*y + 10); %y vector
 
-quiver(u, v)
+quiver(x, y, u, v) %plots quiver with arrow base at x, y and direction u, v
+axis([-2.5 2.5 0.5 5]) %formatting
