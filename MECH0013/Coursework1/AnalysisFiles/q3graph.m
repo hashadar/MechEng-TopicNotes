9c7a6ed0a654484b8a3d2197e2ddd053b793fa39
Hasha Dar <pickupthehouse@gmail.com>
2020-12-06 21:52:04 +0000

    10 ar u

--- MECH0013/Coursework1/AnalysisFiles/q3graph.m
+++ MECH0013/Coursework1/AnalysisFiles/q3graph.m
@@ -1,8 +1,7 @@
 close all
 
-plot(x1, deflection1, x2, deflection2, x3, deflection3)
+plot(x1, deflection1)
 grid on
-title("Direction Deformation y-axis")
+title("Deformation y-axis")
 xlabel("Length [m]")
 ylabel("Deflection [m]")
-legend("Aluminium", "Steel", "Copper")
