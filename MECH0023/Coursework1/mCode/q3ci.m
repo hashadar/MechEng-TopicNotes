a942433740a3a4c162a8713812f72484547d6d76
Hasha Dar <43448050+hashadar@users.noreply.github.com>
2021-12-07 00:32:12 +0000

    23 cw1 u

---
+++ MECH0023/Coursework1/mCode/q3ci.m
@@ -0,0 +1,13 @@
+%HD
+clc
+clear
+close all
+
+%define variables
+k = 1;
+%transfer function
+sys = tf([k 8*k],[0.05 1 -4.05 -81]);
+%plot root locus
+rlocus(sys)
+xlim([-25 15])
+ylim([-30 30])
---
+++ MECH0023/Coursework1/mCode/q3cii.m
@@ -0,0 +1,20 @@
+%HD
+clc
+clear
+close all
+
+%define variables
+k = 1;
+%transfer function
+sys = tf([k 8*k],[0.05 1 -4.05 -81]);
+%plot root locus
+figure1 = figure;
+rlocus(sys)
+xlim([-16 1])
+ylim([-20 20])
+
+%input zeta and wn, vectorise from minimum value to arbitrary value
+zeta = linspace(0.6,0.9,4);
+wd = 7;
+wn = wd./sqrt(1 - zeta.^2);
+sgrid(zeta,wn)
