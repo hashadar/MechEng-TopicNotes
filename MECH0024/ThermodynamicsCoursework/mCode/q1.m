5ee11e85289848ba99c26487f18552a9e9b17371
hashadar <pickupthehouse@gmail.com>
2022-03-25 05:42:50 +0000

    24 cw u

--- MECH0024/ThermodynamicsCoursework/mCode/q1.m
+++ MECH0024/ThermodynamicsCoursework/mCode/q1.m
@@ -2,25 +2,40 @@ clc
 clear
 close all 
 
-%sun orbital characteristics 2022
-%peri = 147105052000;
-%aphe = 152098455000;
-
-%a = (peri + aphe)/2;
-%c = (aphe - peri)/2;
-%b = sqrt(a^2 - c^2);
-
-%orbital equation
-%x = linspace(-peri,aphe,10000);
-%y1 = sqrt(b^2 - ((b^2).*((x-c).^2))./(a^2));
-%y2 = -sqrt(b^2 - ((b^2).*((x-c).^2))./(a^2));
-%plot(x,y1,x,y2)
-%grid on
-
+%at measurement time
 thetaD = -12*(pi/180);
-theta = 38.5*(pi/180);
-phi = 75*(pi/180);
+lati = 51.5;
+theta = (90-lati)*(pi/180);
+timeOfDay = 11;
+phi = ((timeOfDay*360)/24 - 90)*(pi/180);
 
 cosPsi = (cos(thetaD))*(sin(theta))*(sin(phi))+(sin(thetaD))*(cos(theta));
 psi = acos(cosPsi);
 M = sec(psi);
+E = 1353*(0.027+0.81*exp(-0.76*M));
+
+%angles where collector is orientated normal
+thetaDNorm = (-5)*(pi/180);
+latiNorm = 51.5;
+thetaNorm = (90 - latiNorm)*(pi/180);
+timeOfDayNorm = 12;
+phiNorm = ((timeOfDayNorm*360)/24 - 90)*(pi/180);
+alpha = phiNorm - thetaDNorm - thetaNorm;
+
+EAlpha = E*(cos(thetaD)*sin(theta+alpha)*sin(phi) + sin(thetaD)*cos(theta + alpha));
+Area = 920;
+IncEnergy = EAlpha*Area;
+
+%losses
+absorp = 0.97;
+epsi = 0.0941;
+sigma = 5.67e-11;
+TA = 8+273.15;
+TS = 45+273.15;
+h = 0.00256;
+
+%energy bal
+thetaSolar = IncEnergy*absorp;
+thetaConv = Area*h*(TS - TA);
+thetaRad = Area*epsi*sigma*(TS^4);
+powerAvail = thetaSolar - thetaConv - thetaRad; 
