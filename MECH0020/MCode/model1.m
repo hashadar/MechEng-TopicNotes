af4c52a3d9028ebc107e4113d9f8cd321c452a27
Hasha Dar <43448050+hashadar@users.noreply.github.com>
2022-04-25 03:22:33 +0100

    mech0020 updated

--- MECH0020/MCode/model1.m
+++ MECH0020/MCode/model1.m
@@ -15,50 +15,17 @@ a = zeros(numel(raceTrackXY(:,1))-1,1);
 for i = 1:(numel(raceTrackXY(:,1))-1)
     u(i,:) = [(raceTrackXY(i+1,1) - raceTrackXY(i,1)), (raceTrackXY(i+1,2)-raceTrackXY(i,2))];
 end
-temp = numel(raceTrackXY(:,1));
-%generate vector one step ahead of u
-for i = 2:(temp - 1)   
-        v(i,:) = u((i-1),:);
-end
-%clean up vectors and add temp z data
-v(1,:) = u(temp-1,:);
-u = cat(2,u,a);
-v = cat(2,v,a);
-%initialise array for angle
-angleTheta = zeros((temp - 1),1);
-%generate angles 
-for i = 1:temp-1
-    angleTheta(i) = subspace(u(i,:)', v(i,:)');
-    angleTheta(i) = angleTheta(i)*57.2958; %convert to degrees
-end
 
-%plot(linspace(0,4206,numel(angleTheta)),angleTheta)
+%track curvature different method
 
-%find radius of curvature
-%initialise array
-cornerRadii = zeros(numel(raceTrackXY(:,1))-2,1);
-
-for i = 1:(numel(raceTrackXY(:,1))-2)
-    ma = (raceTrackXY(i+1,2) - raceTrackXY(i,2))/(raceTrackXY(i+1,1) - raceTrackXY(i,1));
-    mb = (raceTrackXY(i+2,2) - raceTrackXY(i+1,2))/(raceTrackXY(i+2,1) - raceTrackXY(i+1,1));
-    circleXCoord = (ma*mb*(raceTrackXY(i,2) - raceTrackXY(i+2,2)) + mb*(raceTrackXY(i,1) + raceTrackXY(i+1,1)) - ma*(raceTrackXY(i+1,1) + raceTrackXY(i+2,1)))/(2*(mb-ma));
-    circleYCoord = ma*(circleXCoord - raceTrackXY(i,1)) + raceTrackXY(i,2);
-    cornerRadii(i) = sqrt((raceTrackXY(i,1) - circleXCoord)^2 + (raceTrackXY(i,2) - circleYCoord)^2);
+CR = zeros(numel(raceTrackXY(:,1)) - 2,1);
+for i = 1:numel(raceTrackXY(:,1)) -2
+    a = sqrt((raceTrackXY(i+1,1) - raceTrackXY(i,1))^2 + (raceTrackXY(i+1,2) - raceTrackXY(i,2))^2);
+    b = sqrt((raceTrackXY(i+2,1) - raceTrackXY(i+1,1))^2 + (raceTrackXY(i+2,2) - raceTrackXY(i+1,2))^2);
+    c = sqrt((raceTrackXY(i+2,1) - raceTrackXY(i,1))^2 + (raceTrackXY(i+2,2) - raceTrackXY(i,2))^2);
+    q = (a^2 + b^2 - c^2)/(2*a*b);
+    CR(i,1) = c/(2*sqrt(1-q^2));
 end
-
-for i = 1:(numel(raceTrackXY(:,1))-2)
-    matrixA = [raceTrackXY(i,1), raceTrackXY(i,2), 1;raceTrackXY(i+1,1), raceTrackXY(i+1,2), 1;raceTrackXY(i+2,1), raceTrackXY(i+2,2), 1;];
-    matrixB = - [(raceTrackXY(i,1)^2 + raceTrackXY(i,2)^2), raceTrackXY(i,2), 1; (raceTrackXY(i,1)^2 + raceTrackXY(i+1,2)^2), raceTrackXY(i+1,2), 1; (raceTrackXY(i+2,1)^2 + raceTrackXY(i+2,2)^2), raceTrackXY(i+2,2), 1; ];
-    matrixC = - [(raceTrackXY(i,1)^2 + raceTrackXY(i,2)^2), raceTrackXY(i,1), 1; (raceTrackXY(i,1)^2 + raceTrackXY(i+1,2)^2), raceTrackXY(i+1,1), 1; (raceTrackXY(i+2,1)^2 + raceTrackXY(i+2,2)^2), raceTrackXY(i+2,1), 1; ];
-    matrixD = - [(raceTrackXY(i,1)^2 + raceTrackXY(i,2)^2), raceTrackXY(i,1), raceTrackXY(i,2); (raceTrackXY(i,1)^2 + raceTrackXY(i+1,2)^2), raceTrackXY(i+1,1), raceTrackXY(i+1,2); (raceTrackXY(i+2,1)^2 + raceTrackXY(i+2,2)^2), raceTrackXY(i+2,1), raceTrackXY(i+2,2); ];
-    A = det(matrixA);
-    B = det(matrixB);
-    C = det(matrixC);
-    D = det(matrixD);
-    cornerRadii(i) = sqrt((B^2 + C^2 - 4*A*D)/4*A^2);
-end
-cornerRadii = cat(1,cornerRadii(1),cornerRadii);
-cornerRadii = cat(1,cornerRadii,cornerRadii(numel(cornerRadii)));
 
 %track length
 %initialise vars
@@ -68,8 +35,12 @@ for i = 1:(numel(raceTrackXY(:,1)) -1)
     trackLength(i+1) = trackLength(i)+ L;
 end
 
-%track curvature with length of track
-TCL = cat(2,cornerRadii,trackLength);
+trackLengthTemp = trackLength;
+trackLengthTemp(end,:) = [];
+
+CR = cat(1,CR(1,1),CR(:,1));
+CR = cat(1,CR(:,1),CR(end,1));
+CR = cat(2,CR(:,1),trackLength);
 
 %define time
 t = linspace(0,10,0.01)';
@@ -110,7 +81,94 @@ road.cr = 0.011; %road load coefficient
 veh.mass = veh.curbMass*veh.massFactor + veh.driverMass;
 road.slope = 0; 
 
-input = 1000;
+m       = 200;   
+V = 100;
+rho = 50;
+s1 = 0.5;
+s2      = 0;
+lf       = 1; 
+lr       = 1;
+l       = lf+lr;
+h       = 0.3;
+rsd     = 0.25;
+
+p.tyre1.R0   =       0.5;    % Free tyre radius [m]
+p.tyre1.Fz0  =   4000;        % tyre nominal force [N]
+p.tyre1.kz   = 109000;        % Tyre vertical stiffness [N/m]
+p.tyre1.pCx1 =       1.6;      % Shape factor Cfx for longitudinal force         
+p.tyre1.pDx1 =       0.75;     % Longitudinal friction Mux at Fznom         
+p.tyre1.pDx2 =      -0.05;     % Variation of friction Mux with load         
+p.tyre1.pKx1 =      15;        % Longitudinal slip stiffness Kfx/Fz at Fznom 
+p.tyre1.pCy1 =       1.3;      % Shape factor Cfy for lateral forces
+p.tyre1.pDy1 =       0.7;      % Lateral friction coefficient at Fz0
+p.tyre1.pDy2 =      -0.1;      % Lateral friction coefficient dependency on Fz
+p.tyre1.pEy1 =       0.15126;  % Lateral curvature Efy at Fznom
+p.tyre1.pEy2 =       0.0;      % Lateral curvature dependency on Fz
+p.tyre1.pKy1 =       7.702;    % peak cornering stiffness (Cfa=pKy1*Fz0)
+p.tyre1.pKy2 =       2.13;     % vertical force at which peak Cfa occurs (Fz=pKy2*Fz0)
 
 initialPosVector = [raceTrackXY(2,1) - raceTrackXY(1,1); raceTrackXY(2,2) - raceTrackXY(1,2)];
 normInitialPosVector = initialPosVector/norm(initialPosVector);
+normInitialPosVector = cat(1,normInitialPosVector,0);
+
+%{
+temp = numel(raceTrackXY(:,1));
+%generate vector one step ahead of u
+for i = 2:(temp - 1)   
+        v(i,:) = u((i-1),:);
+end
+%clean up vectors and add temp z data
+v(1,:) = u(temp-1,:);
+u = cat(2,u,a);
+v = cat(2,v,a);
+%initialise array for angle
+angleTheta = zeros((temp - 1),1);
+%generate angles 
+for i = 1:temp-1
+    angleTheta(i) = subspace(u(i,:)', v(i,:)');
+    %angleTheta(i) = angleTheta(i)*57.2958; %convert to degrees
+end
+
+%plot(linspace(0,4206,numel(angleTheta)),angleTheta)
+
+%find radius of curvature
+%initialise array
+cornerRadii = zeros(numel(raceTrackXY(:,1))-2,1);
+
+for i = 1:(numel(raceTrackXY(:,1))-2)
+    ma = (raceTrackXY(i+1,2) - raceTrackXY(i,2))/(raceTrackXY(i+1,1) - raceTrackXY(i,1));
+    mb = (raceTrackXY(i+2,2) - raceTrackXY(i+1,2))/(raceTrackXY(i+2,1) - raceTrackXY(i+1,1));
+    circleXCoord = (ma*mb*(raceTrackXY(i,2) - raceTrackXY(i+2,2)) + mb*(raceTrackXY(i,1) + raceTrackXY(i+1,1)) - ma*(raceTrackXY(i+1,1) + raceTrackXY(i+2,1)))/(2*(mb-ma));
+    circleYCoord = ma*(circleXCoord - raceTrackXY(i,1)) + raceTrackXY(i,2);
+    cornerRadii(i) = sqrt((raceTrackXY(i,1) - circleXCoord)^2 + (raceTrackXY(i,2) - circleYCoord)^2);
+end
+
+for i = 1:(numel(raceTrackXY(:,1))-2)
+    matrixA = [raceTrackXY(i,1), raceTrackXY(i,2), 1;raceTrackXY(i+1,1), raceTrackXY(i+1,2), 1;raceTrackXY(i+2,1), raceTrackXY(i+2,2), 1;];
+    matrixB = - [(raceTrackXY(i,1)^2 + raceTrackXY(i,2)^2), raceTrackXY(i,2), 1; (raceTrackXY(i,1)^2 + raceTrackXY(i+1,2)^2), raceTrackXY(i+1,2), 1; (raceTrackXY(i+2,1)^2 + raceTrackXY(i+2,2)^2), raceTrackXY(i+2,2), 1; ];
+    matrixC = - [(raceTrackXY(i,1)^2 + raceTrackXY(i,2)^2), raceTrackXY(i,1), 1; (raceTrackXY(i,1)^2 + raceTrackXY(i+1,2)^2), raceTrackXY(i+1,1), 1; (raceTrackXY(i+2,1)^2 + raceTrackXY(i+2,2)^2), raceTrackXY(i+2,1), 1; ];
+    matrixD = - [(raceTrackXY(i,1)^2 + raceTrackXY(i,2)^2), raceTrackXY(i,1), raceTrackXY(i,2); (raceTrackXY(i,1)^2 + raceTrackXY(i+1,2)^2), raceTrackXY(i+1,1), raceTrackXY(i+1,2); (raceTrackXY(i+2,1)^2 + raceTrackXY(i+2,2)^2), raceTrackXY(i+2,1), raceTrackXY(i+2,2); ];
+    A = det(matrixA);
+    B = det(matrixB);
+    C = det(matrixC);
+    D = det(matrixD);
+    cornerRadii(i) = sqrt((B^2 + C^2 - 4*A*D)/4*A^2);
+end
+cornerRadii = cat(1,cornerRadii(1),cornerRadii);
+cornerRadii = cat(1,cornerRadii,cornerRadii(numel(cornerRadii)));
+
+%track length
+%initialise vars
+trackLength = zeros(numel(raceTrackXY(:,1))-1,1);
+for i = 1:(numel(raceTrackXY(:,1)) -1)
+    L = sqrt((raceTrackXY(i+1,1) - raceTrackXY(i,1))^2 + (raceTrackXY(i+1,2) - raceTrackXY(i,2))^2);
+    trackLength(i+1) = trackLength(i)+ L;
+end
+
+trackLengthTemp = trackLength;
+trackLengthTemp(end,:) = [];
+
+%track curvature with length of track
+TCL = cat(2,cornerRadii,trackLength);
+ATwL = cat(2,angleTheta,trackLengthTemp); 
+%}
