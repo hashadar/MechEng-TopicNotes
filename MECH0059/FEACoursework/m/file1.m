535a1e7faca084c66f4f726b72ac6be16e03767e
Hasha Dar <43448050+hashadar@users.noreply.github.com>
2022-12-21 00:11:22 +0000

    59 fea u

---
+++ MECH0059/FEACoursework/m/file1.m
@@ -0,0 +1,37 @@
+%% MECH0059 FEA Assignment 2022/2023
+%  UCL Mechanical Engineering
+%  Hasha Dar
+
+clc
+clear
+close all
+
+%% geometry of plate
+
+% SpppppppppppppppppppppppppppppppppF
+%  p      2               3        pF
+%  p              p                pF
+%  p             p p               pF
+%  p      1     p   p     4        pF
+% S0pppppppppppp     pppppppppppppppF
+
+% 0 = origin
+% S = pinned support
+% F = 200MPa tension force applied to right side of plate
+
+height = 30;% height of plate in mm
+width = 70; %width of plate in mm
+theta = deg2rad(30); %angle of crack in degrees
+length2Crack = 35; %length to crack in mm
+heightCrack = 15; %height of crack
+gap = heightCrack*tan(theta/2);
+
+%% definition of elements and nodes
+% 4 4-noded elements
+
+A(:,:,1) = [
+    0,0;
+    0,heightCrack;
+    length2Crack,heightCrack;
+    length2Crack-gap,0;
+    ]; %clockwise
